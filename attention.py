10ac6d566747fcd60a11b415a685805656fa1c14
ZakariaPZ <zaaabc123@gmail.com>
2025-02-03 16:21:29 -0500

    Finish basic attention layer

--- attention.py
+++ attention.py
@@ -1,22 +1,25 @@
 import torch
 from torch import nn
+import math 
+
 
 class Attention(nn.Module):
     
-    def __init__(self):
+    def __init__(self, d_model, d_q, d_k, d_v):
         super().__init__() 
 
         self.softmax = nn.Softmax(dim=1)  # Softmax along rows 
 
-    def forward(self, x):
-        return self.softmax(x @ x.T)
-    
-# Tests
-# 1. Check that rows sum to 1 (softmax)
-# 2. Check that the output is same shape as input
-# 3. Check that the output is symmetric
+        # Projection matrices 
+        self.W_q = nn.Linear(d_model, d_q)
+        self.W_k = nn.Linear(d_model, d_k)
+        self.W_v = nn.Linear(d_model, d_v)
 
-x = torch.randn(3, 3)
-print(x)
-attention = Attention()
-print(attention(x))
+    def forward(self, x):
+        q = self.W_q(x)
+        k = self.W_k(x)
+        v = self.W_v(x)
+
+        scaling_factor = math.sqrt(q.shape[-1])
+
+        return self.softmax(q @ k.T / scaling_factor) @ v
