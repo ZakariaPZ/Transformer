8908fd1b4e32526c15e4291ed60fce49f69706ed
ZakariaPZ <zaaabc123@gmail.com>
2025-02-05 21:43:16 -0500

    Add sinusoidal positional embeddings

--- layers.py
+++ layers.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import math
 
 
 class LayerNorm(nn.Module):
@@ -41,3 +42,22 @@ class PositionWiseFFN(nn.Module):
         x = self.dropout(x)
         x = self.linear2(x)
         return x 
+
+
+class SinusoidalPositionalEncoding(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        
+        positions = torch.arange(10000).float().unsqueeze(1)  # (10000, 1)
+        embedding_indices = torch.arange(0, d_model, 2).float().unsqueeze(0)  # (1, d_model)
+        div_term = torch.exp(-(embedding_indices / d_model) * math.log(10000))  # (1, d_model)
+
+        self.pe = torch.zeros(10000, d_model)
+        # broadcast (10000, 1) and (1, d_model) to get (10000, d_model)
+        self.pe[:, ::2] = torch.sin(positions * div_term)  
+        self.pe[:, 1::2] = torch.cos(positions * div_term)
+
+    def forward(self, x):
+        seq_len = x.size(1)
+        x = x + self.pe[:seq_len, :]
+        return x
