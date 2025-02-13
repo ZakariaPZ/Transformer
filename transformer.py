b55d45fd2e9a0f7627b93e831b764fefbeb11879
ZakariaPZ <zaaabc123@gmail.com>
2025-02-13 01:31:39 -0500

    Add decoder layer

--- transformer.py
+++ transformer.py
@@ -1,8 +1,10 @@
 from torch import nn 
-from layers import LayerNorm, PositionWiseFFN, SinusoidalPositionalEncoding
-from attention import Attention
+from layers import LayerNorm, PositionWiseFFN
+from attention import MultiHeadAttention
+import torch
 
-class TransformerLayer(nn.Module):
+
+class DecoderLayer(nn.Module):
     def __init__(
             self, 
             n_heads,
@@ -12,18 +14,24 @@ class TransformerLayer(nn.Module):
         ):
         super().__init__()
 
-        self.attention = Attention(n_heads, d_model, d_model, d_model, d_model)
+        self.attention = MultiHeadAttention(n_heads, d_model, d_model, d_model, d_model)
         self.layernorm1 = LayerNorm(d_model)
         self.layernorm2 = LayerNorm(d_model)
         self.linear = PositionWiseFFN(d_model, d_hidden, dropout)
+        self.dropout = nn.Dropout(dropout)
     
-    def forward(self, x):
+    def forward(self, x, mask=None):
         # x is shape (seq_len, d_model)
-        attention_output = self.attention(x)
-        # attention_output is shape (seq_len, d_model)
-        residual1 = self.layernorm1(attention_output) + x
-        # residual1 is shape (seq_len, d_model)
-        ffn_output = self.linear(residual1)
+        attention_output = self.attention(x, mask)
+        x = x + self.dropout(attention_output)
+        # x is shape (seq_len, d_model)
+        x = self.layernorm1(x)
+        # x is shape (seq_len, d_model)
+        ffn_output = self.linear(x)
+        x = x + ffn_output
         # ffn_output is of shape (seq_len, d_model)
-        output = self.layernorm2(ffn_output) + residual1
-        return output
+        x = self.layernorm2(x)
+        return x
+
+def create_causal_mask(seq_len):
+    return torch.tril(torch.ones(seq_len, seq_len))
