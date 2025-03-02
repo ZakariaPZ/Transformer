1de3f3b98e1f45e9ccbee3533481396eabd74714
ZakariaPZ <zaaabc123@gmail.com>
2025-03-02 17:55:29 -0500

    Cleanup attention implementation

--- attention.py
+++ attention.py
@@ -1,37 +1,32 @@
-import torch
 from torch import nn
 import math 
 
 
 class MultiHeadAttention(nn.Module):
     
-    def __init__(self, n_heads, d_model, d_q, d_k, d_v):
+    def __init__(self, n_heads, d_head, d_model):
         super().__init__() 
 
         self.n_heads = n_heads
         self.d_model = d_model
-        self.d_q = d_q
-        self.d_k = d_k
-        self.d_v = d_v
+        self.d_head = d_head
+
+        # Each head will have dimension d_heads, and we create n_heads of them
+        # The total dimension will be d_heads * n_heads for query, key and value projections
+        self.W_q = nn.Linear(d_model, d_head * n_heads)  
+        self.W_k = nn.Linear(d_model, d_head * n_heads)
+        self.W_v = nn.Linear(d_model, d_head * n_heads)
+        self.W_o = nn.Linear(d_head * n_heads, d_model)
 
         self.softmax = nn.Softmax(dim=-1)  # Softmax along rows 
 
-        # Projection matrices 
-        # W is of shape (d_model, d_{q,k,v} * n_heads) 
-        # Linear implements Y = XW^T + B
-        self.W_q = nn.Linear(d_model, d_q * n_heads)  
-        self.W_k = nn.Linear(d_model, d_k * n_heads)
-        self.W_v = nn.Linear(d_model, d_v * n_heads)
-        self.W_o = nn.Linear(d_v * n_heads, d_model)
-
     def forward(self, x, mask=None): 
-        batch_size, seq_len, _ = x.size()
-        q = self.W_q(x).reshape(batch_size, seq_len, self.n_heads, self.d_q).transpose(1, 2)  # (batch_size, n_heads, seq_len, d_q)
-        k = self.W_k(x).reshape(batch_size, seq_len, self.n_heads, self.d_k).transpose(1, 2)  # (batch_size, n_heads, seq_len, d_k)
-        v = self.W_v(x).reshape(batch_size, seq_len, self.n_heads, self.d_v).transpose(1, 2)  # (batch_size, n_heads, seq_len, d_v)
+        B, N, _ = x.size()
+        q = self.W_q(x).reshape(B, N, self.n_heads, self.d_head).transpose(1, 2)  # (B, n_heads, N, d_head)
+        k = self.W_k(x).reshape(B, N, self.n_heads, self.d_head).transpose(1, 2)  # (B, n_heads, N, d_head)
+        v = self.W_v(x).reshape(B, N, self.n_heads, self.d_head).transpose(1, 2)  # (B, n_heads, N, d_head)
 
-        scaling_factor = math.sqrt(self.d_q)  # Scaling factor for softmax
-        # q @ k.T is of shape (batch_size, n_heads, seq_len, seq_len)
+        scaling_factor = math.sqrt(self.d_head)  # Scaling factor for softmax
         attention_logits = q @ k.transpose(-1, -2) / scaling_factor
 
         if mask is not None:
@@ -40,7 +35,7 @@ class MultiHeadAttention(nn.Module):
             attention_logits = attention_logits.masked_fill(mask == 0, float('-inf'))
 
         attention_weights = self.softmax(attention_logits)
-        # Multiplying with v is of shape (batch_size, n_heads, seq_len, d_v)
-        y = attention_weights @ v
-        y = y.transpose(1, 2).reshape(y.shape[0], y.shape[2], self.n_heads * self.d_v)  # (batch_size, seq_len, n_heads * d_v)
-        return self.W_o(y)  # (batch_size, seq_len, d_model)
+        y = attention_weights @ v  # (N, N) @ (B, n_heads, N, d_head) -> (B, n_heads, N, d_head)
+        y = y.transpose(1, 2).reshape(B, N, self.n_heads * self.d_head) 
+
+        return self.W_o(y)  # (B, N, d_model)
