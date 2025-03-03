0fcf68c4be1cd47d355599856067e570d0b4d041
ZakariaPZ <zaaabc123@gmail.com>
2025-03-02 19:05:06 -0500

    Update notation for seq len and batch size

--- layers.py
+++ layers.py
@@ -53,9 +53,9 @@ class LayerNorm(nn.Module):
     
     def forward(self, x):
         # Calculate mean and variance of activations of previous layer
-        # Input is of shape (batch_size, seq_len, d_model)
-        mean = x.mean(dim=-1, keepdim=True)  # (batch_size, seq_len, 1)
-        var = x.var(dim=-1, keepdim=True)  # (batch_size, seq_len, 1)
+        # Input is of shape (B, N, d_model)
+        mean = x.mean(dim=-1, keepdim=True)  # (B, N, 1)
+        var = x.var(dim=-1, keepdim=True)  # (B, N, 1)
         
         x = (x - mean) / (torch.sqrt(var) + 1e-6) # normalize activations
         x = self.gamma * x + self.beta  # scale and shift activations
@@ -66,9 +66,8 @@ class PositionWiseFFN(nn.Module):
     def __init__(self, d_model, d_hidden, dropout):
         super().__init__()
 
-        # Input is of shape (batch_size, seq_len, d_model)
-        # Output should have the same shape - we just want to improve the representational
-        # power of the model by adding non-linear transformations
+        # Input is of shape (batch_size, seq_len, d_model). Output should have the same shape - we just 
+        # want to improve the representational power of the model by adding non-linear transformations. 
         self.linear1 = nn.Linear(d_model, d_hidden)
         self.linear2 = nn.Linear(d_hidden, d_model)
 
@@ -99,6 +98,6 @@ class SinusoidalPositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        seq_len = x.size(1)
-        x = x + self.pe[:seq_len, :]
+        N = x.size(1)
+        x = x + self.pe[:N, :]
         return x
